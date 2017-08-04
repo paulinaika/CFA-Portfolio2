627092896435c2475708c96ba019153039bc9eba
Paulina  Sudjatmiko <paulina@192-168-1-100.tpgi.com.au>
2017-08-04 20:18:43 +1000

    content update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,50 +3,50 @@ class PagesController < ApplicationController
   def homepage
     @portfolio=[
       {
-        url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1494038086/projects/calculator_c0dfu0.png',
-        title:'Calculator',
-        description: 'Simple calculator terminal app made using Ruby.',
-        source:'https://github.com/paulinaika/CFA-Project-Calculator',
-        icon:'fa fa-github'
-      },
-
-      {
-        url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1489326688/projects/starwars_xd1txp.png',
-        title:'Star Wars',
-        description: 'Star Wars themed website made using HTML and CSS.',
-        source:'https://codepen.io/paugrammer/pen/qrEvOX',
-        icon:'fa fa-codepen'
-      },
-
-      {
-        url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1489326692/projects/zengarden_yutgwa.png',
-        title:'CSS Zen Garden',
-        description: 'The first attempt on CSS Zen Garden using basic HTML and CSS.',
-        source:'https://github.com/paulinaika/CFA-CSS-Zen',
-        icon:'fa fa-github'
-      },
-
-      {
-        url:'https://res.cloudinary.com/dcbeoroxh/image/upload/v1494244934/projects/animalmatcher_oyydbl.png',
-        title:'Pet Matcher',
-        description: 'This is part of a group project on pet matcher terminal app made using Ruby.',
-        source:'https://github.com/paulinaika/CFA-Pet-Matcher',
-        icon:'fa fa-github'
-      },
-
-      {
-        url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1489326692/projects/trump_ysigmc.png',
-        title:'Trump Page',
-        description: 'A Trump page just for laughter and fun using Ruby on Rails.',
-        source:'https://github.com/paulinaika/CFA-Trump-Page',
-        icon:'fa fa-github'
-      },
-
-      {
         url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1494247127/Screen_Shot_2017-05-08_at_10.38.00_PM_emduru.png',
         title:'Ticked',
-        description: 'A pokedex created for ticks in order to study the distribution of ticks.',
+        description: 'A pokedex for ticks to study the distribution of ticks made using Ruby on Rails',
         source:'https://github.com/paulinaika/CFA-Ticked',
+        icon:'fa fa-github'
+      },
+
+      {
+        url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1500191869/lamb/Screen_Shot_2017-07-16_at_5.54.40_PM.png',
+        title:'Lamb Town',
+        description: 'A forum where users could interact with one another made using Ruby on Rails',
+        source:'https://github.com/paulinaika/CFA-Lamb-Town',
+        icon:'fa fa-github'
+      },
+
+      {
+        url:'https://res.cloudinary.com/dcbeoroxh/image/upload/v1501839364/aquila/Screen_Shot_2017_04_10_at_12_12_27_am.png',
+        title:'Aquila',
+        description: 'A group project to create a study tracker and checklist made using Ruby on Rails',
+        source:'https://github.com/paulinaika/CFA-LetsGetGit',
+        icon:'fa fa-github'
+      },
+
+      {
+        url:'https://res.cloudinary.com/dcbeoroxh/image/upload/v1501838808/Lashes/Screen_Shot_2017-08-04_at_7.18.22_pm.png',
+        title:'Frontend-Lashes',
+        description: 'Frontend user interface for an inventory stock count with API call made using React.Js',
+        source:'https://github.com/paulinaika/CFA-Lash-Frontend',
+        icon:'fa fa-github'
+      },
+
+      {
+        url:'https://res.cloudinary.com/dcbeoroxh/image/upload/v1501838760/Lashes/Screen_Shot_2017-08-04_at_7.10.47_pm.png',
+        title:'Backend-Lashes',
+        description: 'Backend database for an inventory stock count made using MongoDB, Express.Js and Node.Js',
+        source:'https://github.com/paulinaika/CFA-Lash-Backend',
+        icon:'fa fa-github'
+      },
+
+      {
+        url:'http://res.cloudinary.com/dcbeoroxh/image/upload/v1494038086/projects/calculator_c0dfu0.png',
+        title:'Calculator',
+        description: 'My very first application made. A very simple calculator terminal app made using Ruby',
+        source:'https://github.com/paulinaika/CFA-Project-Calculator',
         icon:'fa fa-github'
       }]
   end
